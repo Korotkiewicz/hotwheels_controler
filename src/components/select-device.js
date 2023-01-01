bc333eadd3dcdc9a39a9d0398a5eb180d451fc3e
Michał Korotkiewicz <korotm@gmail.com>
2023-01-01 02:20:13 +0100

    Update select-device.js

--- src/components/select-device.js
+++ src/components/select-device.js
@@ -15,7 +15,7 @@ const SelectDevice = (props: PropsWithDevice): Node => {
       <Text>{props.device.name}</Text>
       <Text>{props.device.localName}</Text>
       <Button
-        title={props.device?.title || props.device?.name || props.device?.localName || 'Unknown'}
+        title={props.device?.title || props.device?.name || props.device?.localName || props.device?.id || 'Unknown'}
         onPress={() => {
           props.setDevice(props.device);
           return navigation.navigate('Options');
