507edc7e24cbbfa66b4a424d8f0dbf3b3ad9c33c
Michal Korotkiewicz <michal.korotkiewicz@smartframe.io>
2023-01-02 11:28:36 +0100

    rssi

--- src/components/select-device.js
+++ src/components/select-device.js
@@ -1,11 +1,22 @@
 import {StyleSheet, Text, TouchableOpacity, useColorScheme, View} from 'react-native';
 import mainStyle from '../styles/main-style';
 import type PropsWithDevice from '../props-with-device';
+import {BleManager} from 'react-native-ble-plx';
+import {useState} from 'react';
 
 const styles = StyleSheet.create(mainStyle);
 
 const SelectDevice = (props: PropsWithDevice): Node => {
   const name = props.device.title || props.device.name || props.device.localName || props.device.id || 'Unknown';
+  const [rssi, setRssi] = useState(null);
+
+  useEffect(() => {
+    device
+      .readRSSI()
+      .then(device => {
+        setRssi(device.rssi);
+      });
+  }, [props]);
 
   return (
     <View style={styles.selectDevice}>
