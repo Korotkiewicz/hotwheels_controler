d5167edd9debb87c0064e1bd5d6b32b3d9b29ee1
Michal Korotkiewicz <michal.korotkiewicz@smartframe.io>
2023-01-03 12:21:30 +0100

    change config

--- src/device-config.js
+++ src/device-config.js
@@ -2,5 +2,5 @@
 export const SERVICE_UUID = '5dfa6919-ce00-4e7c-8ddd-3d7a4060a2e0';
 export const WRITE_CHARACTERISTIC_UUID = '5dfa6919-ce01-4e7c-8ddd-3d7a4060a2e0';
 export const READ_CHARACTERISTIC_UUID = '5dfa6919-ce02-4e7c-8ddd-3d7a4060a2e0';
-export const TURN_LIGHTS_ON_COMMAND = 'A';
-export const TURN_LIGHTS_OFF_COMMAND = 'B';
+export const TURN_LIGHTS_ON_COMMAND = ':LIGHTS_ON;';
+export const TURN_LIGHTS_OFF_COMMAND = ':LIGHTS_OFF;';
