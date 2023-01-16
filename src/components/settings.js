1dc3718913278d18cf45996bca2e9dc4fe48a8f8
Michał Korotkiewicz <korotm@gmail.com>
2023-01-16 09:18:26 +0100

    Create settings.js

---
+++ src/components/settings.js
@@ -0,0 +1,254 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, {useCallback, useRef, useState} from 'react';
+import type {Node} from 'react';
+import {
+  Alert,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  TouchableOpacity,
+  View,
+  useWindowDimensions,
+  Modal,
+  Pressable,
+  TextInput, Dimensions,
+} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import mainStyle from '../styles/main-style';
+import {Characteristic} from 'react-native-ble-plx';
+import {useFocusEffect} from '@react-navigation/native';
+import type PropsWithDeviceAndManager from '../props-with-device-and-manager';
+import {
+  READ_CHARACTERISTIC_UUID,
+  COMMAND_CHARACTERISTIC_UUID,
+  MOVE_CHARACTERISTIC_UUID,
+  COMMAND_GET_INFO,
+  COMMAND_CHANGE_MIN_TURN_PREFIX,
+  COMMAND_CHANGE_MAX_TURN_PREFIX,
+  COMMAND_CHANGE_MIN_THROTTLE_PREFIX,
+  COMMAND_CHANGE_MAX_THROTTLE_PREFIX,
+} from '../device-config';
+import {atob, btoa} from 'react-native-quick-base64';
+
+const Settings: () => Node = (props) => {
+  const [lights, setLights] = useState(false);
+  const [minTurn, setMinTurn] = useState(0);
+  const [maxTurn, setMaxTurn] = useState(180);
+  const [minThrottle, setMinThrottle] = useState(0);
+  const [maxThrottle, setMaxThrottle] = useState(180);
+  const [optionModalVisible, setOptionModalVisible] = useState(false);
+ 
+  
+  const requestDeviceInfo = () => {
+    if (props.commandCharacteristic && props.readCharacteristic) {
+      props.commandCharacteristic
+        ?.writeWithResponse(btoa(COMMAND_GET_INFO))
+        .then(characteristic => {
+          //Alert.alert('Get info request goes');
+        })
+        .catch(error => {
+          Alert.alert('Error getting device info');
+        });
+    }
+  };
+
+  const monitorDevice = (err, update) => {
+    if (err) {
+      console.log(`characteristic error: ${err}`);
+      console.log(JSON.stringify(err));
+    } else {
+      let updateValue = atob(update.value);
+      if (updateValue.startsWith(COMMAND_CHANGE_MIN_TURN_PREFIX)) {
+        setMinTurn(
+          Number(
+            updateValue.substring(
+              COMMAND_CHANGE_MIN_TURN_PREFIX.length,
+              updateValue.indexOf(';'),
+            ),
+          ),
+        );
+      } else if (updateValue.startsWith(COMMAND_CHANGE_MAX_TURN_PREFIX)) {
+        setMaxTurn(
+          Number(
+            updateValue.substring(
+              COMMAND_CHANGE_MAX_TURN_PREFIX.length,
+              updateValue.indexOf(';'),
+            ),
+          ),
+        );
+      } else if (updateValue.startsWith(COMMAND_CHANGE_MIN_THROTTLE_PREFIX)) {
+        setMinThrottle(
+          Number(
+            updateValue.substring(
+              COMMAND_CHANGE_MIN_THROTTLE_PREFIX.length,
+              updateValue.indexOf(';'),
+            ),
+          ),
+        );
+      } else if (updateValue.startsWith(COMMAND_CHANGE_MAX_THROTTLE_PREFIX)) {
+        setMaxThrottle(
+          Number(
+            updateValue.substring(
+              COMMAND_CHANGE_MAX_THROTTLE_PREFIX.length,
+              updateValue.indexOf(';'),
+            ),
+          ),
+        );
+      } else {
+        Alert.alert('Data from HW', updateValue);
+      }
+    }
+  };
+
+  const cancelOptions = () => {
+    setOptionModalVisible(false);
+  };
+
+  const saveOptions = () => {
+    setOptionModalVisible(false);
+    props.commandCharacteristic
+      ?.writeWithResponse(btoa(COMMAND_CHANGE_MIN_TURN_PREFIX + minTurn + ';'))
+      .then(characteristic => {
+        //min turn set correctly
+      })
+      .catch(error => {
+        Alert.alert('Error change min turn', isWorking() ? 'true' : 'false');
+      });
+    props.commandCharacteristic
+      ?.writeWithResponse(btoa(COMMAND_CHANGE_MAX_TURN_PREFIX + maxTurn + ';'))
+      .then(characteristic => {
+        //max turn ser correctly
+      })
+      .catch(error => {
+        Alert.alert('Error change max turn', isWorking() ? 'true' : 'false');
+      });
+    props.commandCharacteristic
+      ?.writeWithResponse(
+        btoa(COMMAND_CHANGE_MIN_THROTTLE_PREFIX + minThrottle + ';'),
+      )
+      .then(characteristic => {
+        //min throttle set correctly
+      })
+      .catch(error => {
+        Alert.alert(
+          'Error change min throttle',
+          isWorking() ? 'true' : 'false',
+        );
+      });
+    props.commandCharacteristic
+      ?.writeWithResponse(
+        btoa(COMMAND_CHANGE_MAX_THROTTLE_PREFIX + maxThrottle + ';'),
+      )
+      .then(characteristic => {
+        //max throttle ser correctly
+      })
+      .catch(error => {
+        Alert.alert(
+          'Error change max throttle',
+          isWorking() ? 'true' : 'false',
+        );
+      });
+  };
+
+  const showOptionsModal = () => {
+    requestDeviceInfo();
+    setOptionModalVisible(true);
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      propos?.readCharacteristic.monitor(monitorDevice);
+    }, [props.readCharacteristic]),
+  );
+
+  return (
+    <>
+      <TouchableOpacity
+            style={[
+              styles.optionButtonTouchable,
+              !props.isWorking ? styles.disabledButton : {},
+            ]}
+            disabled={!props.isWorking}
+            onPress={showOptionsModal}>
+            <View style={styles.optionButton}>
+              <Text style={styles.optionButtonText}>Options</Text>
+            </View>
+      </TouchableOpacity>
+      <View style={styles.modalWrapper}>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={optionModalVisible}
+          onRequestClose={() => {
+            setOptionModalVisible(!optionModalVisible);
+          }}>
+          <View>
+            <View style={[styles.modalView, {marginTop: statusBarHeight + 50}]}>
+              <Text style={styles.modalText}>Adjust steerage:</Text>
+              <View style={styles.inputWrapper}>
+                <Text style={styles.inputText}>Left Max turn:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={setMinTurn}
+                  keyboardType="numeric">
+                  {minTurn}
+                </TextInput>
+              </View>
+              <View style={styles.inputWrapper}>
+                <Text style={styles.inputText}>Right Max turn:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={setMaxTurn}
+                  keyboardType="numeric">
+                  {maxTurn}
+                </TextInput>
+              </View>
+              <View style={styles.inputWrapper}>
+                <Text style={styles.inputText}>Min Throttle:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={setMinThrottle}
+                  keyboardType="numeric">
+                  {minThrottle}
+                </TextInput>
+              </View>
+              <View style={styles.inputWrapper}>
+                <Text style={styles.inputText}>Max throttle:</Text>
+                <TextInput
+                  style={styles.input}
+                  onChangeText={setMaxThrottle}
+                  keyboardType="numeric">
+                  {maxThrottle}
+                </TextInput>
+              </View>
+              <View style={styles.centeredView}>
+                <Pressable
+                  style={[styles.modalButton]}
+                  onPress={() => cancelOptions()}>
+                  <Text style={styles.modalButtonTextStyle}>Cancel</Text>
+                </Pressable>
+                <Pressable
+                  style={[styles.modalButton, styles.modalButtonPrimary]}
+                  onPress={() => saveOptions()}>
+                  <Text style={styles.modalButtonTextStyle}>Save</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </Modal>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create(mainStyle);
+
+export default Settings;
